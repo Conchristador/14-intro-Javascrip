aaf11b08a3621191d663afc9b8b1c32872e3e900
Christopher Sloat <44382012+Conchristador@users.noreply.github.com>
2019-03-12 16:01:50 -0400

    Updated

--- 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -1,14 +1,14 @@
-let tableData = data;
+var tableData = data;
 
-let tbody = d3.select("tbody");
+var tbody = d3.select("tbody");
 
 function table(data) {
   tbody.html("");
 
   data.forEach((dataRow) => {
-    let row = tbody.append("tr");
+    var row = tbody.append("tr");
     Object.values(dataRow).forEach((val) => {
-      let ufoInfo = row.append("td");
+      var ufoInfo = row.append("td");
         ufoInfo.text(val);
       }
     );
@@ -18,8 +18,8 @@ function table(data) {
 function buttonClick() {
 
  d3.event.preventDefault();
-  let date = d3.select("#datetime").property("value");
-  let filteredData = tableData;
+  var date = d3.select("#datetime").property("value");
+  var filteredData = tableData;
   if (date) {
     filteredData = filteredData.filter(row => row.datetime === date);
   }
